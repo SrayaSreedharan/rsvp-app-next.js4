4c5cfb20881b7ab94a21487635825b31d4df8a27
SRAYA SREEDHARAN <srayasreedharan111@gmail.com>
2025-08-15 20:58:40 +0530

    Add email validation and disable submit until valid

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,7 @@ import {Container, TextField,
   Button, 
   Typography,
   Table, 
-  TableBody, 
-  TableCell, TableHead, TableRow,Dialog, DialogTitle, DialogContent, Slide} from "@mui/material";
+  TableBody, TableCell, TableHead, TableRow,Dialog, DialogTitle, DialogContent, Slide} from "@mui/material";
 
 export default function Home() {
   const [name, setName] = useState("");
